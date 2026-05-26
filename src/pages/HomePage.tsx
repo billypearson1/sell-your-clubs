4225692505e377431219cedcfd96b58fc35dd736
billypearson1 <billy.pearson1@outlook.com>
2026-05-26 09:59:59 +0000

    reduce hero further

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
             <img
               src={golfbag}
               alt="Golf bag"
-              className="h-[220px] w-[220px] max-w-[90vw] rounded-full border-4 border-white object-cover shadow-[0_20px_45px_rgba(0,0,0,0.12)] sm:h-[340px] sm:w-[340px]"
+              className="h-[180px] w-[180px] max-w-[90vw] rounded-full border-4 border-white object-cover shadow-[0_20px_45px_rgba(0,0,0,0.12)] sm:h-[260px] sm:w-[260px]"
             />
           </div>
         </div>
